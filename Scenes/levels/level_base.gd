0770845df7265da0a1c3e902d15efa85730911e7
HyoYongAnn <dksgydyd0617@naver.com>
2024-08-31 20:22:33 +0900

    init

---
+++ Scenes/levels/level_base.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+func _ready():
+	$Items.hide()
+	$Player.reset($SpawnPoint.position)
+	set_camera_limits()
+	spawn_items()
+	
+	$Player.life_changed.connect($CanvasLayer/HUD.update_life)
+	score_changed.connect($CanvasLayer/HUD.update_score)
+
+
+func set_camera_limits():
+	var map_size = $World.get_used_rect()
+	var cell_size = $World.tile_set.tile_size
+	$Player/Camera2D.limit_left = (map_size.position.x - 5) * cell_size.x
+	$Player/Camera2D.limit_right = (map_size.end.x + 5) * cell_size.x
+	
+signal score_changed
+
+var item_scene = load("res://Scenes/item.tscn")
+
+var score = 0: set = set_score
+
+func spawn_items():
+	var item_cells = $Items.get_used_cells(0)
+	for cell in item_cells:
+		var data = $Items.get_cell_tile_data(0, cell)
+		var type = data.get_custom_data("type")
+		var item = item_scene.instantiate()
+		add_child(item)
+		item.init(type, $Items.map_to_local(cell))
+		item.picked_up.connect(self._on_item_picked_up)
+
+func _on_item_picked_up():
+	score += 1
+
+func set_score(value):
+	score = value
+	score_changed.emit(score)
+
+
+func _on_player_died():
+	GameState.restart()
